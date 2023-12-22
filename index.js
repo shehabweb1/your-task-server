359ac54a5ca9b1439015e36f75bd94aa6b867f65
MD Shehab Hossen <shehab.web1@gmail.com>
2023-12-22 13:06:15 +0600

    JWT setup

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
-app.use(cors())
- 
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@learning.axf2sgn.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -23,6 +27,27 @@ async function run() {
     
     const userCollection = client.db("yourTask").collection("users");
 
+    app.post('/jwt', async(req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: 3600000})
+      res.send({token});
+    });
+
+    const verifyToken = (req, res, next) => {
+      if(!req.headers.authorization){
+        return res.status(401).send({message: "unauthorized access"});
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if(err){
+          return res.status(401).send({message: "unauthorized access"})
+        }
+        req.decoded = decoded;
+        next();
+      });
+    }
+
+
     app.post("/api/users", async (req, res)=>{
       const cursor = req.body;
       const result = await userCollection.insertOne(cursor);
@@ -33,6 +58,8 @@ async function run() {
       const result = await userCollection.find(cursor).toArray();
       res.send(result);
     })
+
+
 
 
   } finally {
